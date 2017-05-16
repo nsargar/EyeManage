241a2fd4b9f003c7998f796a9441beb1df7b8a76
Omkar Nikam <omkar.nikam24@gmail.com>
2017-05-16 18:37:43 +0530

    Removed Unwanted code

--- src/main/java/com/app/eyemanage/EyeManageApplication.java
+++ src/main/java/com/app/eyemanage/EyeManageApplication.java
@@ -1,17 +1,9 @@
 package com.app.eyemanage;
 
-import javax.annotation.PostConstruct;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-import com.app.eyemanage.pojo.UserPOJO;
-import com.app.eyemanage.service.UserService;
 
 @SpringBootApplication
 @EnableAutoConfiguration
