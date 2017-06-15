f4846b5406304455773c2721cbf1b20525167be8
Omkar Nikam <omkar.nikam24@gmail.com>
2017-06-15 11:04:50 +0530

    Create EyeManageApplication.java

--- src/main/java/com/app/eyemanage/EyeManageApplication.java
+++ src/main/java/com/app/eyemanage/EyeManageApplication.java
@@ -7,20 +7,10 @@ import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 @EnableAutoConfiguration
-//@EnableJpaRepositories
 @Configuration
 public class EyeManageApplication {
-	
 
 	public static void main(String[] args) {
 		SpringApplication.run(EyeManageApplication.class, args);
-	
-		
-//		user.save(new UserPOJO("Nilis08", "nilis", "voidprongzz", "ggfjyskjf.asdg@dhf.com"));
 	}
-	
-/*	  @Bean
-	    public SessionFactory sessionFactory(HibernateEntityManagerFactory hemf) {
-	        return hemf.getSessionFactory();
-	    }*/
 }
